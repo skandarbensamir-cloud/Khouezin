09b365e1bfd18d245fd1942d5a0c41907b6f3181
skander <skandarbensamir@outlook.com>
2026-09-11 10:34:11 +0200

    added Account class

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿namespace simplegit;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace simplegit;
 
 class Program
 {
     static void Main()
     {
         Console.WriteLine("Hello, Git");
+        Console.WriteLine(Dosomething("skander"));
     }
+
+
+    static string Dosomething(string name)
+    {
+        return $"Hello {name}";
+    
+    }
+
 }
+
+
