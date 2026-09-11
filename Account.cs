ad0d53a718883c89459887c3564c341be4ffc518
skander <skandarbensamir@outlook.com>
2026-09-11 10:20:35 +0200

    initial commit

---
+++ Account.cs
@@ -0,0 +1,6 @@
+﻿namespace simplegit;
+
+public class Account
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace simplegit;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, Git");
+    }
+}
